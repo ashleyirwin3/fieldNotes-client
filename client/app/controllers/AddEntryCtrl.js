e68ddff778db27bc25e909883789da752d1c748f
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-05-16 10:22:54 -0500

    put entries on scope

--- client/app/controllers/AddEntryCtrl.js
+++ client/app/controllers/AddEntryCtrl.js
@@ -4,15 +4,4 @@ app.controller('AddEntryCtrl', function($scope, EntryFact) {
     $scope.entries= res.data
     console.log($scope.entries)
   })
-// $scope.editing = false
-
-// const getEntries = () => {
-//   EntryFact.getAll()
-//     .then((entries) => {
-//       console.log(entries)
-//       $scope.entries = entries.entries
-//       $scope.$apply()
-//     })
-//   }
-
 })
