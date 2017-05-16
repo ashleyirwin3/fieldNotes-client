5666d45ba100e5721d336054b98b3be5e9a5d7c1
Ashley Irwin <ashleykayirwin3@gmail.com>
2017-05-16 10:21:59 -0500

    wrote functino to get all entries from db

--- client/app/factories/EntryFact.js
+++ client/app/factories/EntryFact.js
@@ -1,14 +1,13 @@
 app.factory('EntryFact', function($http) {
-
-  const server = `http://localhost:3000/api/v1`
-
-  return {
-    getAll: function() {
-      return new Promise((resolve, reject) => {
-        $http.get(`${server}/profile`)
-          .then((data) => {
-            resolve(data.data)
-          })
+  return{
+    getEntries : () => {
+      return $http
+      .get(`http://localhost:3000/api/v1/profile`)
+      .then((res) => {
+          return res
+      })
+      .catch((res) => {
+          console.error(res);
       })
     }
   }
